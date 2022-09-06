1927ce55a73924021cbfc6e912cdc39d2945be6b
theo lindqvist <theo.lindqvist@elev.ga.ntig.se>
2022-09-06 18:10:21 +0200

    ASCII Art

--- pragermarang/pragermarang/Program.cs
+++ pragermarang/pragermarang/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Välkommen till Den Fega Riddarens flykt!");
+﻿Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Välkommen till Den Fega Riddarens flykt!");
 Console.WriteLine("Är du redo att börja?");
 string start = Console.ReadLine().ToLower();
 while (start != "ja" && start != "nej")
@@ -14,6 +15,41 @@ if (start == "ja")
     string win = ("0");
     while (active == "1")
     {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine(@"
+............''',,;:cloooooddxxkkxkkkkkkkkOOO00KKKKKKKKXXXKKKKKKKKKKKK00KKKKKKKK000000OOOkkkxdxxxdddoollccc:::;;,,,''....
+...........''',,,;:clodddddxxkkkkkkkkkOOOO000KKKKKKKXXXXXXXXKKKKKKKKKKKKKKKKKKK000000OOOOOkkkkkxxxddoollcccc::;;,,,''...
+...........''',,;;clloodddxxkkkkkkOOOOOOO000KKKKXXXXXXXXXXXXXXXXXXXXXKKKKKKKKKKK0000000OOOOOOkkkxxxddoolllllcc::;,,'''..
+............',,,;:clooddxxxkkkkkOOOOOOOO0000KXXXXXXXXXXXXXXXXXXXXXXXXXKKKKKKKKKK0000000000OOOOkkxxxdddoolllllcc::;,,''..
+..........''',,;:clooodxxxdooooooooooodddxk0KXXXXXXXXXXXNXXXXXXXXXXXXKXXXKKKKKOxddddoooooooddddxxxxdddoolccccc:::;,,,'..
+.........''',,;;:cloooddxxl               o0KXXXXXXXXXNNNNNXXXXXXXXXKKXXXKKXKKx                xkxxdddoolccccc::;;,,''..
+.........'',,,;;:ccloodddxl               o0XXXXXXXXXNNNNNNNNNNNXXXXXXXXXKKKKKx                xxxxddooollccc:::;,,,''..
+..........'',,,;::clodddxxl               l0XXXXXXXXNNNNNXXXXXXXXXXXXXXXXKKKKKx                dxxxddddoolllcc::;;,,''..
+..........''',;;:clloddxxxl               cOXXXXXXXXXXXXXXXXXXXXXXXXXXXXXKKKKKd                dxddddddooollccc::;;,''..
+..........'',,;;;:clloodxxl               :OXXXXXXXXXXXXXXXXXNNXXXXXXXXXKKKKKKd                oddddddoooolllcc:::;,,'..
+.........''',,,;;:ccllooddc               ;OXXXXXXXXXXXXXXXXXXXXXXXXXXXXKKXKKKd                odooooooooollllcc:;;,,'''
+ ........''',,,;::ccllooddc               ;OXXXXXXXXXXXXXXXXXXXXXXXXXXKKKKXKKKd                ldoooooolllllllcc:;;,,'''
+  ........'',,,;;:ccloodddc               ,OXXXKXXXXXXXXKkOKKKKKXXXKKKKKKKKKKKo                looooollllccccccc:;,''...
+  ........''',,;;:cllooddd:               ,OKKKKKXKKKKKXx   KXKKKKKKKKKKKKKK00o                looollllcccc::c:::;;,''..
+  ........''',,;;:cclloooo:               ,OKKKKKKKKKKKOd   xO0KKKKKKK00KKKK00o                clllllcc:::::::::;;;,''..
+  .........''',,;;:clllooo:               ,kK0000K0KKxc       :oOKK00000K0000Ol                cllllcc:::;;;;;;,,;,,''..
+ ...........'',,,,:ccccllo:               ,k0000000kl            d0K00000OOOOOl                cllc:::::;;;,,,,,,,''''..
+   ..........''',,;;::clll;               'x0000000  cl        o  d00000OkOOkkc                cc::;;;;;;,,,,,,''''''...
+    .........''',,,;;:cccl;               'x0OO0000  cd        x  k000OOOOkkxxc                ::;;;;,,,,,''''''''......
+    .........'''',,,;;:::c,               'dOOOOO0OOxo          ck000OOOOOkkxxc                ;:;;;,,,,''''''''........
+.............'''',,,,;;;;:,...............,dxxxkkkkkOkl        xkkkkkkkxxxxxddc                ;:;;;;,,,,,''''''''''''..
+;;;;;:::;;::::ccc::::cccccc:::;;;::::::::::llllloooool:       llllllllllollolccccccccccccccccclollllllllllllllcllllllll
+cccccccllclllccccclllllllllclllllloooollllllooooooooolc   l   cddooooooooddoddoooooodddddddddddddddxxxxddddddddddddddddd
+looooooooooooooodddddooooooodddxddoodddddodxxdxxdddddoc  dxc  cxxxxxddxddkkxxkxxxxxxxxxxkkxkkxxxxxxkkkOOOOkkkxxxxkkkkkkk
+xdddddddddddxkxxdddddddxxxkkkkkkxxxxxxxxxkkkkkkkxxxxxd  o0Ol  ckkkOkxxkkxkOkkOOkkkOkkkkkkOOOOOkkxxkkkkOO00000OOkkkkkkOOO
+xxxxxxxkkxkkkkxxxxxxxxkkOkOOOkkkkxkkkkkOOOOOOOkkkkxkOd  xKOl  ckOOOOkkOOkO00OO00OkO0OOOOOO00OO0OOkkOOOOOO00KK0000OOkkkOO
+xkOkkOOkkkkkkkkkkkkOOOOOOOOOOkkkkkkkkO00OO00OkkOkxkOOo  dxdc  xx000OkkOOOO0K000KK0O00OkOOOO0000000000OOO0OOO0KKKKK00OOOO
+OOkO0OkOkkkkOkkkOO0OO00OOkOOkkkOOOO00OOO000OkkOOkkOOxc  dxxo   lk000OkO0OO0KK0O0KK0O00OOOOOOO0K000KK0O000kkkO0KKKKK00000
+xOOxkkkkxkOkxO0OOOO0OOOOkOOkkkOOO000OO00OOkOOOOkkkkkkxdkOOOkdoodk000kkO0OOO0K000000OO00OOO00OO0K0OO0K0OOOOkkOOOOO00KK000
+xokOkxxOkxkOOOOOOkkOOOkOOOkxkOOk0KK0OOKKOxk00OOkOOkkOKKKOOKK00OO00OkkOOOkO0KKK0O000OO0000OOO0OOO000OO00OOOOOOOOOkk0KK0OK
+");
+        Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine("Du befinner dig i ett slott, till höger och vänster om dig finns två dörrar.");
         Console.WriteLine("Vilken dörr tar du?");
         string valEtt = Console.ReadLine().ToLower();
@@ -25,6 +61,38 @@ if (start == "ja")
         }
         if (valEtt == "vänster")
         {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(@"
+........................................................................................................................
+..............,,,,,,,...................................................................................................
+........................................................................................................................
+........'.............'..                             .... .....                             ........      .............
+........'................                         ...'''''.',,,,'...                         .....         ..'..........
+...'...'..................      .              ..''''''..'.',',,'..'...              ..     ......         .............
+..''..'..''.'.............            .....   .,,','''''''.',,,'.....''.   .....            ......         '............
+'.'''..'''''..............            ....  ...'......................'...  ....            ......         '''...'......
+..'''...''''..............                  ............'...''............                  ......         ''...........
+..'''....'''..............                  ...........''...'..............                 ......         ''....''.....
+''.'...''.'...............                 .................'............'.                 ......         .'''.....''..
+.......'..................                 .....  .........................                 ......         .............
+..''''.''.'...............                 ...........................'....                 ......         '.'..........
+''.....''.................                 ................................                 ......         ..'......'...
+''.....''.................                 ................................                 ......         .............
+..''.....'................                 ................................                 ......         .............
+..''....''...............                  ......                    ......                  .....         '......'.....
+'''''.................                        .  ...          ..                               ...         .............
+'.''............             ............................................................                  .............
+..''.........          ...............................'..''....................................            .............
+..........          ...................'''''',,,,'',,,,,,,,,,,,,,,,,'''''''''.....................          ............
+......       .....................'''.'''''''',,,''''''',,,,,,'',,'',,''''''.............................      .........
+...     .....................................''.''..''..'''''''''.'..........................................        ...
+      ....................................'.......'......'''''.''''..............................................       
+...........................''...''...'.''''''..........''''''''..''''''''.''....'...................................... 
+...........................'...''''...''.''''....'.......''''......''...'''...''........................................
+................................'''''.....''''....'..............'..'.........'.................'.......................
+");
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Du kommer ut i en hall, med en stor port längst fram och en dörr vid höger sida av hallen");
             Console.WriteLine("Tar du dörren till höger eller porten?");
             string valTvå = Console.ReadLine().ToLower();
@@ -36,17 +104,115 @@ if (start == "ja")
             }
             if (valTvå == "porten")
             {
+                Console.Clear();
                 Console.WriteLine("Du kom ut ur slottet och vann spelet!");
                 win = ("1");
             }
             else if (valTvå == "höger")
             {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(@"
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMX  NMMMMNXNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWK KWMMMWK KWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMWWWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN  KWWNK   NMMMMWWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMWX KKXNWWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMW    KK     XMMMWK NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMWX    KXXXNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNXXK            KNNX   WMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMWX    KKKKKXNWMMWWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN   c;,',;c              KNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMNK        KXXXNWWWMMMMMMMMMMMMMMMMMMMMMMMMMWX l;'.........,:l          KNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMWNXK          XXNWMMMMMMMMMMMMMMMMMMMMWNK l,................,;:cl     XWWWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMWX          KXWMMMMMMMMMMMMMMMMWK c;'.........................',;: WWWWMMWNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMWN          XNWMWWWMMMMMMMMMMW  :'............................ ' WWWWX  NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMWNK        KXK KNMMMMMMMMMMMMN  c''......'''.';:cl  l;.......:XWW c.:KMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMWX          XWWNNWMMMMMMMMMMNK                 NN c'....., N ,... WWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMWN        KKKKKXWMMMMMMMMNK ;'          '  :KXX :.....:  '....' XWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMNMM    :    MMMNMMMMMMMMNX ' '         ; .lKXXX ,.';;::........;l    KXWMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMWNNXNM           NMMMMMMWNK ;                 K ;;    ;.............'',c WMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWN         WMMMMWNKKXNN                K       ,................., MMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWX        XWMX  KNNX   llc:::c l: KKKK      ,...................:KMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWX         K      l:        KN 'cKK  l lc;..''..................,  WMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWX            :,, NN   NNNXK ,'lc,..,'....l ,...................':   KNWMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNXXXX        c,,;;'.':cc:;'..';cc     l   ;........................,;cl  NMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWK          l:;,,,;:cll              l...........................,l XWMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMX       ll       KXXXKKK            c'.........................; XWMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXXN ll         KNXK           lc:;;;,'......................, XMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMW            KXK           ll c;;;;;'....................c WMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWW                      lc   c;;;;'.................' NMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMX ll                  l::   l:;;;'...............; WMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMW                      ll     lc:;,............' XMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN  l           KK      XNXK         :,......,  WMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWK              K       NNNXK   NMMMWX      NMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMW                       XNNXXXXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWK                         XWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN                      KKK    NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN ;;::::ccc   ll     llc::::::c  WMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWN c'''''.....',;c   c'.....''''',,; NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMWWWMMMMMMMMMMMMMMMMMMWXK   ;'''........'  XX ;.......,:l     WMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMWNWWWWWMMMMMMMMMMMMMMMNK       c;'....,  XWMMMMN ;..';c        KWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMWWWWWWWWMMMMMMMMMMMMMMWK   KK          XMMMMMMMMMMN l           KNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMWWWWWWMMMMMMMMMMMMMX             XMMMMMMMMMMMMMW             NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMWWWWWWMMMMMMMMMMMMMWK           KWMMMMMMMMMMMMMMW            NWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMWWWWWWWWWMMMMMMMMMWX          NMMMMMMMMMMMMMMMMW           XWWWWMWWMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMWWWWWNNWMMMMMMMMMWK         WMMMMMMMMMMMMMMMMMN         KNWWWWNNWMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNWMWWMMMMMMMMMWX        XMMMMMMMMMMMMMMMMMMW         KWWWNNWWNWMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWWNWWWWMMMMMWX        NMMMMMMMMMMMMMMMMMMMN        KWMMWWWWNWWWWMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNWWNWWWMMMWX       KMMMMMMMMMMMMMMMMMMMMMX       KWMMMMMWWNWNNWMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWWWNWWWMWK       WMMMMMMMMMMMMMMMMMMMMMWK      KNMMMMMMWWWWWWWWWMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWNNWWWN        WMMMMMMMMMMMMMMMMMMMMMMW      KXWMMMMMMMWWWWNWWWMWMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWNWWNX         NWWWWWWWWWWWWWWWWWWWWWWX       KKNWWWWWWWWWNNWWNNNWMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWWWWWWNNNNXXXK         NNXXXXXXXXXXXXXXXXXXXK          KXNNNNNNNNNNNNNNWWWWMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMWWWWNNNNXXXXXXXK             KK                            KXXKKKKKXXXXXXXNNNNWWWWMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMWWWNNXXXKK                                                   KK       KKKKXXXNNWWWMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMWWWNNXXKKK                                                      K      KKKKXXNNNWWWMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMWWWNNNNXXXKKKKKKKKK                                                 KKKXXXXXNNWWWWMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWWWNNNNNNNNNNXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXKK    KNNNNNNNWWWWWMMMMMMMMMMMMMM
+");
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Bakom dörren fanns den arga kungen som avrättade dig!");
                 Console.WriteLine("Du förlorade!");
             }
         }
         else if (valEtt == "höger")
         {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(@"
+MMMMMMMMMMMMMMMMMMMMMWk:xNWMK; OWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMK; xNMO;.;dKWMWWMMMMMMMMMMMMMMMW0oodkKNNXXXNNWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMNc. ld:  co::cOWMMMMMMMMMMMMMMMMNk:;cloc ;;;::cclloodkO0KNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMXc.... ;  ;   oNMMMMMMMMMMMMMMMMMMO  cc; ;;;;;      ..   :cloxOKNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMM0;    ;     :; :OWMMMMMMMMMMMMMMMMNl.    ;;    ;    ;;;    ...  :ldkKWMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMWWWMMMMMMMMMMMMMx  ; .. ;  .....;0WMMMMMMMMMMMMMMMMXc.;;       ;    ;:cllc::;     .. :oOXMMMMMMMMMMMMMMMMMMMMMMMMM
+N0xkdlclloxOXWMMMMMMMMO; ; . ;; ..  ;   dXMMMMMMMMMMMMMMMMd  ; .    ;oxl;    ;cddddolc:;    . ;oONMMMMMMMMMMMMMMMMMMMMMM
+dc; .  ;;  ;:ldk0XWMMXo    ;;   . :; ;; ;0WMMMMMMMMMMMMMMMx    .  .  :OKOd:    ;ldddddddlc:;   . :dXMMMMMMMMMMMMMMMMMMMM
+NWKoc:; ;;;;;;  ;:loxo    ;    .;kKd  ; :KMMMMMMMMMMMMMMMWo ;         oKKK0d:    :okxddddddol:;   . oKWMMMMMMMMMMMMMMMMM
+MMMMWXOdc;;;;   ;;;    . ;     ;lkxxx:   dWMMMMMMMMMMMMMM0; :  ;   ;  :OKKKK0d:    ckOkxdddddddl:   . oKMMMMMMMMMMMMMMMM
+MMMMMMMMNOo:;;     ;;;; ...  .cklckKKl ;  dNMMMMMMMMMMMMNo    cl  :o;  dKKKKKKOo:    o0KOkdddddddol;  . dXMMMMMMMMMMMMMM
+MMMMMMMMMMWKxc;;;     ;;  ...    lKK0l ; .:0WMMMMMMMMMMWk   .:ko  lOc  c0KKKKKK0ko:   :xKK0kddddddddo: ..;kWMMMMMMMMMMMM
+MMMMMMMMMMMMMXxc;;      ;;   ;;  lkkkl ;  xWMMMMMMMMMMM0;   :xKl  oKo  ;kKKKKKKKKOxl;  ;o0KK0xddddddddoc ..lXMMMMMMMMMMM
+MMMMMMMMMMMMMMWXd: ;;     ;;;;  okxOKd ;; cXMMMMMMMMMMK:   o0K0c  xXx;  xKKKKKKKKK0kdc;  cOKKKOxddddddddoc .;0WMMMMMMMMM
+MMMMMMMMMMMMMMMMWKo; ;      ; .;OXK0kc ;; .:0WMMMMMMM0:   l0KXk; :kXO:  o0KKKKKKKKKKOdo:  :xKKK0xdddddddddo:. kWMMMMMMMM
+MMMMMMMMMMMMMMMMMMWOc  ;     .;odkkkkc ;; .. l0WMMMWO:   l0KKKx  c0X0c  lOKKKKKKKKKKK0xdl;  dKKK0kddddddddddl  kWMMMMMMM
+MMMMMMMMMMMMMMMMMMMMNx: ;;  . x0kOKX0l ;; ..   dXMNx;   o0KKKKl  dKK0l  ckKKKKKKKKKKKK0kdoc  o0KKKOddddddddddl.:NMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMKo  ; .;OXK0Okx: ;;; ..   :dc ; ;dKKKKXO:.:OKKKo  ck0KKKKKKKKKKKKKkddl  l0KKKOxddddddoc :0MMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMWO: . ldkkkkOk: ;;;; ..    ;; :kKKKK0Ol  dKKKKd  cxOKKKKKKKKKKKKKKOxdo; c0KKKOxdddl: :xNMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMXd.:0OO0KKK0c ;;;;;    ;;  lOKKK0kdl;.:OKKKKd  cdkKKKKKKKKKKKKKKK0xddc c0KKKOdc;;lONMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMWo:0KKKKKXOc ;;;;  ;;;  ;dOOkkxddc   lk0KKKd  ldx0KKKKKKKKKKKKKKK0kddl c0KKk;:xXWMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMWd d0KK0Okx: ;;;;   ..... .. ;;:l . cddxkOOl. lddOKKKKKKKKKKKKKKKKKkddl c0O;cXWMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMOcoxkkkO0Kd ;;;;;;  ..  .... .... cdddddddl  oddk0KKKKKKKKKKKKKK0kxdddo:lc;0MMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMNolOk0KKKKx   ;;;;;; .        ...   ;:coddl  odddk0KKKKKKKKKKK0kddl:;;;; .dWMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMk;dKKKKOkdc . ;;;;; ..             ...  :c ;dddooodkO0KKKK0Oxl:;;:ldkO000XMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMNo lkkxk0kdoc... ;;; ..       .  ;;;;; .... ;   ....  ;;;;;  ..;o0NWMMMMMMMMMMMMMMMMMMMMMMWN
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMM0:..;kK0xddl . ..    ....     ....  ;; ..  ;;::;  ......        ;clodxOO0KXXNNNNNNNXK0OxdOX
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMM0:  . clclc;:;. . ; .;:  ;      . ..  ; :0WWWWWNX0kdl: ..        ;;     ;;::cccccc:; ;cd0NM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMWx ; .cK0dc  ; . .;; .;:;llcc:ldooxOl    oNMMMMMMMMMMWNKkdoc:;                  ;:coxOXWMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMWO:  .cXMMMWX0kdl  ;  .  .;dk0XWMMWWKl ; ;OMMMMMMMMMMMMMMMMMWNX0OkxdddooooooddxkOKXWMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMWKkdkkd: ;  kMMMMMMWNKx: ; ..  .oWMMMMWNX0o;;; lNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMM0:;; .      dNMMMMMXx: .    .. .cKMMMNOoc;      oWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMNl.cl;;looocxNMMMMMKo: .; .  .:dxXMMMWkc:;  c  ; xWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMWX0XNKXWMMMMMMMMMMM0:co:;llc;oXMMMMMMW0dO0kld0klkWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+");
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Bakom den högra dörren fanns en drake som åt upp dig!");
             Console.WriteLine("Du förlorade!");
         }
